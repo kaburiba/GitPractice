ef62b50c9e9cedb107c17772462163ee0372b6f6
show <s.kubota.sys22@morijyobi.ac.jp>
2023-01-17 14:29:33 +0900

    ログイン機能の新規作成

---
+++ src/main/java/dao/AccountDAO.java
@@ -0,0 +1,156 @@
+package dao;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dto.Account;
+import dto.KadaiAccount;
+import util.GenerateHashedPw;
+import util.GenerateSalt;
+
+public class AccountDAO {
+
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+	    String username = dbUri.getUserInfo().split(":")[0];
+	    String password = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+	    return DriverManager.getConnection(dbUrl, username, password);
+	}
+	
+	public static int registerAccount(Account account) {
+		String sql = "INSERT INTO kadaiaccount VALUES(default, ?, ?, ?, ?, current_timestamp)";
+		int result = 0;
+		
+		// ランダムなソルトの取得(今回は32桁で実装)
+		String salt = GenerateSalt.getSalt(32);
+		
+		// 取得したソルトを使って平文PWをハッシュ
+		String hashedPw = GenerateHashedPw.getSafetyPassword(account.getPassword(), salt);
+		
+		
+		
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setString(1, account.getName());
+			pstmt.setString(2, account.getMail());
+			pstmt.setString(3, salt);
+			pstmt.setString(4, hashedPw);
+
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(result + "件更新しました。");
+		}
+		return result;
+	}
+	
+	// メールアドレスを元にソルトを取得
+	public static String getSalt(String mail) {
+		String sql = "SELECT salt FROM kadaiaccount WHERE mail = ?";
+		
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setString(1, mail);
+
+			try (ResultSet rs = pstmt.executeQuery()){
+				
+				if(rs.next()) {
+					String salt = rs.getString("salt");
+					return salt;
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	// ログイン処理
+	public static KadaiAccount login(String mail, String hashedPw) {
+		String sql = "SELECT * FROM kadaiaccount WHERE mail = ? AND password = ?";
+		
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setString(1, mail);
+			pstmt.setString(2, hashedPw);
+
+			try (ResultSet rs = pstmt.executeQuery()){
+				
+				if(rs.next()) {
+					
+					String name = rs.getString("name");
+					String age = rs.getString("age");
+					String gender = rs.getString("gender");
+					String tell = rs.getString("tell");
+					
+					String salt = rs.getString("salt");
+					
+					return new KadaiAccount( name,age,gender, tell,mail, salt, null, null);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public static int KadairegisterAccount(KadaiAccount account) {
+		String sql = "INSERT INTO Kadaiaccount VALUES( ?, ?, ?, ?,?,?,?)";
+		int result = 0;
+		
+		// ランダムなソルトの取得(今回は32桁で実装)
+		String salt = GenerateSalt.getSalt(32);
+		
+		// 取得したソルトを使って平文PWをハッシュ
+		String hashedPw = GenerateHashedPw.getSafetyPassword(account.getPassword(), salt);
+		
+		
+		
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setString(4, account.getGender());
+			pstmt.setString(1, account.getName());
+			pstmt.setString(6, salt);
+			pstmt.setString(7, hashedPw);
+			pstmt.setString(2, account.getAge());
+			pstmt.setString(5, account.getMail());
+			pstmt.setString(3, account.getTell());
+
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(result + "件更新しました。");
+		}
+		return result;
+	}
+}
